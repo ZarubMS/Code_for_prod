6c7dae7f14754d53a0a0f2061808231d65ce5417
MZarubin <zarubinms@gmail.com>
2023-08-05 13:31:59 +0300

    как -то так

--- mostrans_appmetrica.py
+++ mostrans_appmetrica.py
@@ -1,9 +1,6 @@
-import sys
 import os
-import json
 from datetime import datetime, timedelta
 import csv
-import requests
 from CronBase import CronBase
 
 
@@ -14,46 +11,52 @@ path_file = os.path.realpath(__file__)
 
 class mostrans_appmetrica(CronBase):
 	
-	#получение параметров из конфига
-	def get_params_loader(self, req_type, i, j):
+	#получение параметров из конфига для запросов 1ого и 2ого типов
+	def get_params_loader(self, request_type, id, parametr):
 		params = {}
 		headers = self.config["headers"]
 		url = self.config["url"]
 		self.timeout = self.config["timeout"]
+
+		#Получаем общие параметры взависимости от типа запроса
+		params = {**self.config["request_params"]["common"], **self.config["request_params"][request_type]}
 		params["date1"] = self.date_from
 		params["date2"] = self.date_to
 
-		if req_type == 1:
-			params = self.config["first_request_params"]["const_params"]
-			params["id"] = self.config["first_request_params"]["variable_params"]["ids"][i]
-			params["metrics"] = self.config["first_request_params"]["variable_params"]["metrics"][j]
-		elif req_type == 2:
-			params = self.config["second_request_params"]["const_params"]
-			params["id"] = self.config["second_request_params"]["variable_params"]["ids"][i]
-			row_ids1 = str(self.config["second_request_params"]["row_ids"][j])
-			if j == 0:
-				row_ids2 = row_ids1[:1] + "[null]," + row_ids1[1:]
+		#Получаем изменяемые параметры
+		#для запроса 1ого типа
+		if request_type == "first_request_type":
+			params["id"] = id
+			params["metrics"] = parametr
+			params["row_ids"] = self.config["row_ids"][0]
+		#для запроса 2ого типа
+		elif request_type == "second_request_type":
+			params["id"] = id
+			params["metrics"] = self.config["metrics"][1][0]
+			self.count_metrics = len(parametr)
+			if [None] in parametr:
+				row_ids = str(parametr).replace("[None]", "[null]").replace(" ", "").replace("\'", "\"")
 			else:
-				row_ids2 = row_ids1
-			row_ids = row_ids2.replace(" ", "").replace("\'", "\"")
+				row_ids = str(parametr).replace(" ", "").replace("\'", "\"") 
 			params["row_ids"] = row_ids
-			self.count_metrics = len(self.config["second_request_params"]["row_ids"][j])
-		
 		request_params = dict(url = url, headers = headers, params = params)
 		main_params = dict(request_params = request_params)
 		return main_params 
 			
-	#парсим JSON
-	def read_json(self, req_type, load_data):
+	#парсим JSON для запросов 1ого и 2ого типов
+	def read_json(self, request_type, load_data):
 		data = []
-		if req_type == 1:
+		#Если JSON возвращен запросом 1ого типа
+		if request_type == "first_request_type":
 			data.append(load_data['query']['date1'])
 			data.append(load_data['query']['date2'])
 			data.append(load_data['time_intervals'][0][0])
 			data.append(load_data['query']['ids'][0])
 			data.append(load_data['query']['metrics'][0])
 			data.append(load_data['totals'][0][0])
-		elif req_type == 2:
+
+		#Если JSON возвращен запросом 2ого типа	
+		elif request_type == "second_request_type":
 			for i in range(self.count_metrics - 1):
 				for j in range(24):
 					a = []
@@ -68,38 +71,38 @@ class mostrans_appmetrica(CronBase):
 
 	
 	def write_csv(self, data, file_path):
-		date = datetime.now()
-		date = datetime.strftime(date, "%Y%m%d")
 		columns = self.config["csv_headers"]
 		with open (file_path, 'w', newline='') as f:
 			wr = csv.writer(f)
 			wr.writerow(columns)
 			wr.writerows(data)
-		print("Запись завершена в ", file_path)
+		
 	
 	def main_logic(self):
 		data_list = []
-		#запросы для каждого id и 2ух метрик
-		req_type = 1
-		for i in range(3):
-			for j in range(2):
-				main_params = self.get_params_loader(req_type, i, j)
+		ids = self.config["ids"]
+		
+		#запросы 1ого типа для каждого id и 2ух метрик формируем по запросу
+		metrics = self.config["metrics"][0]
+		for id in ids:
+			for metric in metrics:
+				main_params = self.get_params_loader("first_request_type", id, metric)
 				params = self.load_request(main_params)
 				self.response_dttm = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 				response = params["response"]
 				load_data = response.json()
-				data = self.read_json(req_type, load_data)
+				data = self.read_json("first_request_type", load_data)
 				data_list.append(data)
-	
-		#запросы для каждого id и всяких разных метрик
-		req_type = 2
-		for i in range(3):
-			for j in range(2):
-				main_params = self.get_params_loader(req_type, i, j)
+
+		#запросы 2ого типа для каждого id и 2ух наборов метрик формируем по запросу
+		row_ids_list = self.config["row_ids"][1]
+		for id in ids:
+			for row_ids in row_ids_list:
+				main_params = self.get_params_loader("second_request_type", id, row_ids)
 				params = self.load_request(main_params)
 				response = params["response"]
 				load_data = response.json()
-				data = self.read_json(req_type, load_data)
+				data = self.read_json("second_request_type", load_data)
 				data_list = data_list + data
 		return data_list
 		
@@ -120,7 +123,6 @@ class mostrans_appmetrica(CronBase):
 			pass
 		
 
- 
 
 		
 
